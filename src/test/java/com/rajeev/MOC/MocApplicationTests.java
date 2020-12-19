da56fcfb9d0a68b718af3c9cb665bef38c9021f3
rajeevsachdeva <rsachdeva516@gmail.com>
2020-12-19 12:10:01 +0800

    new project added

---
+++ src/test/java/com/rajeev/MOC/MocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajeev.MOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
